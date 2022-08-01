2b5bcaa8f11681bb2bb812ee3b64a39f37a2bf02
ashokDev10 <imashokdev@gmail.com>
2022-08-01 17:37:58 +0530

    datatype

---
+++ dataType.js
@@ -0,0 +1,15 @@
+let firstName = "hash"; // String
+
+let age = 24; // Integer
+
+let isYoung = true; // Boolean
+
+let lastName = undefined; // undefined
+
+let trophy = null; // null
+
+console.log("Result...", typeof firstName);
+console.log("Result...", typeof age);
+console.log("Result...", typeof isYoung);
+console.log("Result...", typeof lastName);
+console.log("Result...", typeof trophy);
