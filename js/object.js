1b789e8e524c978016c74e892a2a9bfdff7da4b4
ashokDev10 <imashokdev@gmail.com>
2022-08-01 17:46:29 +0530

    object

---
+++ js/object.js
@@ -0,0 +1,17 @@
+// Object
+let person = {
+  name: "Hash man",
+  age: 23,
+  isAlive: true,
+  gender: "male",
+  address: "Ranipet",
+  sibling: {
+    brother: "ashik",
+  },
+};
+
+// Do Notation
+console.log(person.isAlive);
+
+// Bracket Notation
+console.log(person["sibling"].brother);
---
+++ js/variables.js
@@ -0,0 +1,12 @@
+// var
+// let
+// const
+
+var studentName = "hash";
+console.log("Welcome....", studentName);
+
+let studentName2 = "hash2";
+console.log("Welcome....", studentName2);
+
+const studentName3 = "hash3";
+console.log("Welcome....", studentName3);
