b615b1a65582669a844c08eb80618b299d6bbf60
ashokDev10 <imashokdev@gmail.com>
2022-08-02 10:53:05 +0530

    operator

---
+++ js/operator.js
@@ -0,0 +1,25 @@
+// Relational Operator
+
+let x = 5;
+
+console.log(x < 5);
+console.log(x <= 5);
+
+console.log(x > 5);
+console.log(x >= 5);
+
+// Equality Operator
+
+console.log(x === 5);
+console.log(x !== 5);
+
+// string comparison
+console.log("Alen" > "Anbu"); // dictionary
+
+// comparison of different type
+
+console.log("1" < 5); // 1<5
+console.log(true == 1);
+
+// 1 - True
+// 0 - False
